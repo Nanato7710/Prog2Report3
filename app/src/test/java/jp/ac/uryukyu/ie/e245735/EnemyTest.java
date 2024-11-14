83a959ec7e8b32d9b3dc8abcfcb2dd2412fd7720
Kazusa KINJO <e245735@ie.u-ryukyu.ac.jp>
2024-11-14 13:09:05 +0900

    attackTestの修正
    Heroの攻撃が弱すぎて一撃ではEnemyが倒れないことがあった。
    それをEnemyが倒れるまでHeroに攻撃させるように変更した。

--- app/src/test/java/jp/ac/uryukyu/ie/e245735/EnemyTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e245735/EnemyTest.java
@@ -17,7 +17,9 @@ class EnemyTest {
         int defaultHeroHp = 100;
         Hero demoHero = new Hero("デモ勇者", defaultHeroHp, 100);
         Enemy slime = new Enemy("スライムもどき", 10, 100);
-        demoHero.attack(slime);
+        while (!slime.getDead()) {
+            demoHero.attack(slime);
+        }
         slime.attack(demoHero);
         assertEquals(defaultHeroHp, demoHero.getHitPoint());
     }
